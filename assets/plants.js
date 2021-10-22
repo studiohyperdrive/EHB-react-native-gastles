2f56d27b2e57ad9104e3ff01cc2022ce75bb23c5
Jochem Janssens <jochem.janssens@studiohyperdrive.be>
2021-10-22 16:46:56 +0200

    feat: add start project

---
+++ assets/plants.js
@@ -0,0 +1,36 @@
+export const plants = [{
+    name: "Pannekoekenplant",
+    image: "https://www.groenvanbijons.be/sites/default/files/styles/content_small/public/iStock-1029914886.jpg?itok=h1UxJn_5",
+    water: "normaal",
+    sun: "halfschaduw"
+  },
+  {
+    name: "Aloë Vera",
+    image: "https://cdn.webshopapp.com/shops/271987/files/357391427/650x650x2/aloe-vera-barbadensis.jpg",
+    water: "droog",
+    sun: "halfschaduw"
+  },
+  {
+    name: "Araucaria",
+    image: "https://www.groenvanbijons.be/sites/default/files/styles/slider_large/public/Araucaria.jpg?itok=BmRHxTnI",
+    water: "droog",
+    sun: "halfschaduw"
+  },
+  {
+    name: "Gatenplant",
+    image: "https://www.groenvanbijons.be/sites/default/files/styles/slider_large/public/monstera_0.jpg?itok=q5I29imX",
+    water: "normaal",
+    sun: "schaduw"
+  },
+  {
+    name: "Agave",
+    image: "https://www.groenvanbijons.be/sites/default/files/styles/slider_large/public/Agave%20stricta_510x768.jpg?itok=aU3do03X",
+    water: "droog",
+    sun: "zon"
+  },
+  {
+    name: "Koffieplant",
+    image: "https://www.groenvanbijons.be/sites/default/files/styles/slider_large/public/Knipsel_4.JPG?itok=nD816ez_",
+    water: "normaal",
+    sun: "zon"
+  }];
---
+++ components/Plant/Plant.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const Plant = () => {
+    return (
+        <View>
+            <Text>Text</Text>
+        </View>
+    );
+}
