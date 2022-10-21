e9b49a560fe4c5bc9e3b5fd9dd46c00be130e463
Jochem Janssens <jochem.janssens@vrt.be>
2022-10-21 09:51:50 +0200

    chore: update assets

--- assets/plants.js
+++ assets/plants.js
@@ -1,36 +1,36 @@
 export const plants = [{
-    name: "Pannekoekenplant",
-    image: "https://www.groenvanbijons.be/sites/default/files/styles/content_small/public/iStock-1029914886.jpg?itok=h1UxJn_5",
-    water: "normaal",
-    sun: "halfschaduw"
-  },
-  {
-    name: "Aloë Vera",
-    image: "https://cdn.webshopapp.com/shops/271987/files/357391427/650x650x2/aloe-vera-barbadensis.jpg",
-    water: "droog",
-    sun: "halfschaduw"
-  },
-  {
-    name: "Araucaria",
-    image: "https://www.groenvanbijons.be/sites/default/files/styles/slider_large/public/Araucaria.jpg?itok=BmRHxTnI",
-    water: "droog",
-    sun: "halfschaduw"
-  },
-  {
-    name: "Gatenplant",
-    image: "https://www.groenvanbijons.be/sites/default/files/styles/slider_large/public/monstera_0.jpg?itok=q5I29imX",
-    water: "normaal",
-    sun: "schaduw"
-  },
-  {
-    name: "Agave",
-    image: "https://www.groenvanbijons.be/sites/default/files/styles/slider_large/public/Agave%20stricta_510x768.jpg?itok=aU3do03X",
-    water: "droog",
-    sun: "zon"
-  },
-  {
-    name: "Koffieplant",
-    image: "https://www.groenvanbijons.be/sites/default/files/styles/slider_large/public/Knipsel_4.JPG?itok=nD816ez_",
-    water: "normaal",
-    sun: "zon"
-  }];
+  name: "Pannekoekenplant",
+  image: "https://boompjevoorthuis.be/wp-content/uploads/2020/09/flora_5ef36e3d2e749-scaled.jpeg",
+  water: "normaal",
+  sun: "halfschaduw"
+},
+{
+  name: "Aloë Vera",
+  image: "https://cdn.webshopapp.com/shops/271987/files/357391427/650x650x2/aloe-vera-barbadensis.jpg",
+  water: "droog",
+  sun: "halfschaduw"
+},
+{
+  name: "Araucaria",
+  image: "https://www.fleur.nl/media/contentmanager/content/Web_Araucaria%20heterophylla%202.png",
+  water: "droog",
+  sun: "halfschaduw"
+},
+{
+  name: "Gatenplant",
+  image: "https://www.ikea.com/be/nl/images/products/fejka-kunstplant-binnen-buiten-monstera__0959226_pe809439_s5.jpg?f=xl",
+  water: "normaal",
+  sun: "schaduw"
+},
+{
+  name: "Agave",
+  image: "https://cdn.webshopapp.com/shops/271987/files/323453921/agave-angustifolia.jpg",
+  water: "droog",
+  sun: "zon"
+},
+{
+  name: "Koffieplant",
+  image: "https://media.s-bol.com/qjr58L3ApkE2/550x597.jpg",
+  water: "normaal",
+  sun: "zon"
+}];
